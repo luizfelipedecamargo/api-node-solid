66f47e80f2bb63213e8a8b6cda0cf13b3f834494
Luiz Felipe de Camargo <LF.CAMARGO@GMAIL.COM>
2024-04-15 14:54:11 -0300

    Initial commit

---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
+
+export class CreateUserUseCase {
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider,
+  ) {}
+
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+
+    if (userAlreadyExists) {
+      throw new Error('Usuário ja existe.');
+    }
+
+    const user = new User(data);
+
+    await this.usersRepository.save(user);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Enviando email de from',
+        email: 'lf.camargo@gmail.com',
+      },
+      subject: 'Agora sim, email enviado com sucesso',
+      body: '<p>Email enviado com sucesso.</p>'
+    })
+  }
+}
